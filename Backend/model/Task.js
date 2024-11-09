840c57eff6c8d021c695cfa4afd578ff77dbd2d9
Tushar75R <mylmye07@gmail.com>
2024-11-09 22:47:45 +0530

    Created Schema and Models

---
+++ Backend/model/Task.js
@@ -0,0 +1,35 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    priority: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Task = model("Task", taskSchema);
